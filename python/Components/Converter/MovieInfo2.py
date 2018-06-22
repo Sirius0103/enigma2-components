1cc2ad71c76decd39eb86f5c4135e9739c030d3a
Sirius0103 <sirius.altech@gmail.com>
2018-06-22 21:48:21 +0300

    update

---
+++ python/Components/Converter/MovieInfo2.py
@@ -0,0 +1,231 @@
+# -*- coding: UTF-8 -*-
+#Coders by Sirius
+# v1.1
+
+from Components.Converter.Converter import Converter
+from Components.Element import cached, ElementError
+from Components.config import config
+from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr, eServiceReference
+from ServiceReference import ServiceReference
+from Tools.Directories import fileExists
+import os, sys
+
+class MovieInfo2(Converter, object):
+	MOVIE_SHORT_DESCRIPTION = 0 # meta description when available.. when not .eit short description
+	MOVIE_META_DESCRIPTION = 1 # just meta description when available
+	MOVIE_REC_SERVICE_NAME = 2 # name of recording service
+	MOVIE_REC_FILESIZE = 3 # filesize of recording
+	MOVIE_ORIG_NAME = 4
+	MOVIE_COVER_NAME = 5
+	MOVIE_FULL_DESCRIPTION = 6
+	MOVIE_DESCRIPTION = 7
+	MOVIE_DIRECTOR = 8
+	MOVIE_ACTORS = 9
+	MOVIE_COUNTRY = 10
+	MOVIE_GENRE = 11
+	MOVIE_YEAR = 12
+	MOVIE_RATING = 13
+	MOVIE_INFO = 14
+
+	def __init__(self, type):
+		Converter.__init__(self, type)
+		if type == "ShortDescription":
+			self.type = self.MOVIE_SHORT_DESCRIPTION
+		elif type == "MetaDescription":
+			self.type = self.MOVIE_META_DESCRIPTION
+		elif type == "FullDescription":
+			self.type = self.MOVIE_FULL_DESCRIPTION
+		elif type == "Description":
+			self.type = self.MOVIE_DESCRIPTION
+		elif type == "Director":
+			self.type = self.MOVIE_DIRECTOR
+		elif type == "Actors":
+			self.type = self.MOVIE_ACTORS
+		elif type == "Coutry":
+			self.type = self.MOVIE_COUNTRY
+		elif type == "Genre":
+			self.type = self.MOVIE_GENRE
+		elif type == "Year":
+			self.type = self.MOVIE_YEAR
+		elif type == "Rating":
+			self.type = self.MOVIE_RATING
+		elif type == "MovieInfo":
+			self.type = self.MOVIE_INFO
+		elif type == "MovieName":
+			self.type = self.MOVIE_ORIG_NAME
+		elif type == "CoverName":
+			self.type = self.MOVIE_COVER_NAME
+		elif type == "RecordServiceName":
+			self.type = self.MOVIE_REC_SERVICE_NAME
+		elif type == "FileSize":
+			self.type = self.MOVIE_REC_FILESIZE
+		else:
+			raise ElementError("'%s' is not <ShortDescription|MetaDescription|RecordServiceName|FileSize> for MovieInfo converter" % type)
+
+	@cached
+
+	def getText(self):
+		name = ''
+		name2 = ''
+		filename = ''
+		filetext = ''
+		fulldescription = ''
+		description = ''
+		director = ''
+		actors = ''
+		coutry = ''
+		genre = ''
+		year = ''
+		rating = ''
+		movieinfo = ''
+		moviename = ''
+		covername = ''
+		service = self.source.service
+#		info = self.source.info
+		event = self.source.event
+
+		if service:
+			if isinstance(service, iPlayableServicePtr):
+				info = service and service.info()
+				name = info.getName()
+				name2 = config.movielist.last_videodir.value
+				filename = '%s%s' % (name2, name)
+				filetext = filename.split('.')[0].strip() + '.txt'
+			else: # selection
+				info = service and self.source.info
+				if (service.flags & eServiceReference.flagDirectory) == eServiceReference.flagDirectory:
+					name = service.getPath()
+					filename = '%s' % (name)
+					filetext = filename + filename.split('/')[-2].strip() + '.txt'
+				else:
+					name = service.getPath()
+					filename = '%s' % (name)
+					filetext = filename.split('.')[0].strip() + '.txt'
+			try:
+#ShortDescription
+				if self.type == self.MOVIE_SHORT_DESCRIPTION:
+					return (info.getInfoString(service, iServiceInformation.sDescription)
+						or (event and event.getShortDescription())
+						or service.getPath())
+#MetaDescription
+				elif self.type == self.MOVIE_META_DESCRIPTION:
+					return ((event and (event.getExtendedDescription() or event.getShortDescription()))
+						or info.getInfoString(service, iServiceInformation.sDescription)
+						or service.getPath())
+#FullDescription
+				elif self.type == self.MOVIE_FULL_DESCRIPTION:
+					if fileExists(filetext):
+						fulldescription = open("%s" % filetext, "r").readlines()[2]\
+							+ open("%s" % filetext, "r").readlines()[3]\
+							+"\n"+ open("%s" % filetext, "r").readlines()[4]\
+							+"\n"+ open("%s" % filetext, "r").readlines()[6]\
+							+ open("%s" % filetext, "r").readlines()[1]\
+							+ open("%s" % filetext, "r").readlines()[5]
+						return fulldescription
+					else:
+						return ((event and (event.getExtendedDescription() or event.getShortDescription()))
+							or info.getInfoString(service, iServiceInformation.sDescription)
+							or service.getPath())
+#Description
+				elif self.type == self.MOVIE_DESCRIPTION:
+					if fileExists(filetext):
+						description = open("%s" % filetext, "r").readlines()[4]
+						return description
+					else:
+						return ((event and (event.getExtendedDescription() or event.getShortDescription()))
+							or info.getInfoString(service, iServiceInformation.sDescription)
+							or service.getPath())
+#Director
+				elif self.type == self.MOVIE_DIRECTOR:
+					if fileExists(filetext):
+						director = open("%s" % filetext, "r").readlines()[2]
+					else:
+						director = _("No Director")
+					return director
+#Actors
+				elif self.type == self.MOVIE_ACTORS:
+					if fileExists(filetext):
+						actors = open("%s" % filetext, "r").readlines()[3]
+					else:
+						actors = _("No Actors")
+					return actors
+#Coutry
+				elif self.type == self.MOVIE_COUNTRY:
+					if fileExists(filetext):
+						coutry = open("%s" % filetext, "r").readlines()[1]
+					else:
+						coutry = _("No Coutry")
+					return coutry
+#Genre
+				elif self.type == self.MOVIE_GENRE:
+					if fileExists(filetext):
+						genre = open("%s" % filetext, "r").readlines()[5]
+					else:
+						genre = _("No Genre")
+					return genre
+#Year
+				elif self.type == self.MOVIE_YEAR:
+					if fileExists(filetext):
+						year = open("%s" % filetext, "r").readlines()[6]
+					else:
+						year =  _("No Year")
+					return year
+#Rating
+				elif self.type == self.MOVIE_RATING:
+					if fileExists(filetext):
+						ratingall = open("%s" % filetext, "r").readlines()[7]
+						rating = ratingall.split('Рейтинг:')[1].split('.')[0].strip()
+					else:
+						rating = ""
+					return rating
+#MovieInfo
+				elif self.type == self.MOVIE_INFO:
+					if fileExists(filetext):
+						movieinfo = open("%s" % filetext, "r").readlines()[7]
+					else:
+						movieinfo = _("No Info")
+					return movieinfo
+#MovieName
+				elif self.type == self.MOVIE_ORIG_NAME:
+					if fileExists(filetext):
+						nameall = open("%s" % filetext, "r").readlines()[0]
+						moviename = nameall.split('Оригинальное название:')[1].strip()
+					else:
+						moviename = _("No Name")
+					return moviename
+#CoverName
+				elif self.type == self.MOVIE_COVER_NAME:
+					path1 = '/media/usb/covers/' + filetext.split('/')[-1].split('.')[0].strip() + '.jpg'
+					path2 = '/media/hdd/covers/' + filetext.split('/')[-1].split('.')[0].strip() + '.jpg'
+					path3 = '/media/sdb1/covers/' + filetext.split('/')[-1].split('.')[0].strip() + '.jpg'
+					path4 = '/media/sdb2/covers/' + filetext.split('/')[-1].split('.')[0].strip() + '.jpg'
+					if fileExists(path1) or fileExists(path2) or fileExists(path3) or fileExists(path4):
+						covername = filetext.split('/')[-1].split('.')[0].strip()
+					else:
+						covername = filetext.split('/')[-2].strip()
+					return covername
+#MovieServiceName
+				elif self.type == self.MOVIE_REC_SERVICE_NAME:
+					rec_ref_str = info.getInfoString(service, iServiceInformation.sServiceref)
+					return ServiceReference(rec_ref_str).getServiceName()
+#FileSize
+				elif self.type == self.MOVIE_REC_FILESIZE:
+					if (service.flags & eServiceReference.flagDirectory) == eServiceReference.flagDirectory:
+						return _("Directory")
+					filesize = info.getInfoObject(service, iServiceInformation.sFileSize)
+					if filesize is not None:
+						if filesize >= 104857600000: #100000*1024*1024
+							return _("%.0f GB") % (filesize / 1073741824.0)
+						elif filesize >= 1073741824: #1024*1024*1024
+							return _("%.2f GB") % (filesize / 1073741824.0)
+						elif filesize >= 1048576:
+							return _("%.0f MB") % (filesize / 1048576.0)
+						elif filesize >= 1024:
+							return _("%.0f kB") % (filesize / 1024.0)
+						return _("%d B") % filesize
+#End
+			except:
+				return _("No Text")
+		return ""
+
+	text = property(getText)
---
+++ python/Components/Renderer/MovieCover.py
@@ -0,0 +1,108 @@
+# -*- coding: UTF-8 -*-
+#Coders by Nikolasi
+# v1.1
+
+from Renderer import Renderer
+from Tools.Directories import fileExists
+from Tools.LoadPixmap import LoadPixmap 
+from Tools.Directories import SCOPE_SKIN_IMAGE, SCOPE_CURRENT_SKIN, fileExists, resolveFilename
+from enigma import ePixmap, eServiceCenter, loadPic, eTimer, ePicLoad
+from Components.Converter.Poll import Poll
+from Components.Pixmap import Pixmap 
+import os
+
+class MovieCover(Renderer, Poll):
+	__module__ = __name__
+	searchPaths = ('/media/hdd/%s/', '/media/usb/%s/', '/media/sdb1/%s/', '/media/sdb2/%s/')
+
+	def __init__(self):
+		Poll.__init__(self)
+		Renderer.__init__(self)
+		self.path = 'covers'
+		self.nameCache = {}
+		self.size = []
+		self.pics = []
+		self.pixmaps = []
+		self.pngname = ''
+		self.png = ''
+		self.picon = ePicLoad()
+		self.pixdelay = 60
+
+	def applySkin(self, desktop, parent):
+		attribs = []
+		for (attrib, value) in self.skinAttributes:
+			if attrib == 'path':
+				self.path = value
+			elif attrib == 'size':
+				self.size = value.split(',')
+			elif attrib == 'pixdelay':
+				self.pixdelay = int(value)
+			attribs.append((attrib, value))
+		self.skinAttributes = attribs
+		return Renderer.applySkin(self, desktop, parent)
+
+	GUI_WIDGET = ePixmap
+
+	def changed(self, what):
+		self.poll_interval = 2000
+		self.poll_enabled = True
+		if self.instance:
+			pngname = ''
+			if (what[0] != self.CHANGED_CLEAR):
+				sname = self.source.text
+				pngname = self.nameCache.get(sname, '')
+				if (pngname == ''):
+					pngname = self.findPicon(sname)
+					if (pngname != ''):
+						self.nameCache[sname] = pngname
+			if (pngname == ''):
+				pngname = self.nameCache.get('default', '')
+				if (pngname == ''):
+					pngname = self.findPicon('picon_default')
+					if (pngname == ''):
+						tmp = resolveFilename(SCOPE_CURRENT_SKIN, 'no_poster.png')
+						if fileExists(tmp):
+							pngname = tmp
+						self.nameCache['default'] = pngname
+			if (self.pngname != pngname):
+				self.pngname = pngname
+				self.instance.setScale(1)
+			self.picon.setPara((int(self.size[0]), int(self.size[1]), 1, 1, False, 1, '#00000000'))
+			self.picon.startDecode(self.pngname, 0, 0, False)
+			self.png = self.picon.getData()
+			self.instance.setPixmap(self.png)
+			self.runAnim()
+
+	def findPicon(self, serviceName):
+		for path in self.searchPaths:
+			try:
+				name = ((path % self.path) + serviceName)
+				pngname = name.split('.')[0].strip() + '.jpg'
+				if fileExists(pngname):
+					return pngname
+			except:
+				return ''
+		return ''
+
+	def runAnim(self):
+		txt=[]
+		text = ""
+		if len(self.pics) == 0:
+			for x in self.pixmaps:
+				self.pics.append(loadPic(resolveFilename(SCOPE_SKIN_IMAGE, x), int(self.size[0]), int(self.size[1]), 0, 0, 0, 1))
+			self.slide = len(self.pics)
+			self.timer = eTimer()
+			self.timer.callback.append(self.timerEvent)
+			self.timer.start(self.pixdelay, True)
+		else:
+			self.instance.setPixmap(self.png)
+		
+	def timerEvent(self):
+		if self.slide > 0:
+			self.timer.stop()
+			self.instance.setPixmap(self.pics[len(self.pics) - self.slide])
+			self.slide = self.slide - 1
+			self.timer.start(self.pixdelay, True)
+		else:
+			self.timer.stop()
+			self.instance.setPixmap(self.png)
