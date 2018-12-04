cf5880b23758fa0e3b47a84eb54ebff241c7b09e
Sirius0103 <sirius.altech@gmail.com>
2018-12-04 16:34:10 +0300

    update

--- python/Components/Converter/ServiceInfoEX.py
+++ python/Components/Converter/ServiceInfoEX.py
@@ -26,8 +26,7 @@ from Tools.Directories import fileExists
 
 if fileExists("/etc/issue"):
 	image = ''
-	line = open(("/etc/issue"), "r").readlines()
-	for text in line:
+	for text in open("/etc/issue"):
 		image += text
 		if not 'openpli' in image:
 			codec_data = {-1: ' ', 0: 'MPEG2', 1: 'MPEG4', 2: 'MPEG1', 3: 'MPEG4-II', 4: 'VC1', 5: 'VC1-SM', 6: 'HEVC'}
