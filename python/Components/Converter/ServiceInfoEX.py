320e88cd2fe6a415cb7ddd03e698fe8dc67fa166
Sirius0103 <sirius.altech@gmail.com>
2018-12-25 06:57:44 +0300

    update

--- python/Components/Converter/ServiceInfoEX.py
+++ python/Components/Converter/ServiceInfoEX.py
@@ -27,12 +27,11 @@ from Tools.Directories import fileExists
 
 if fileExists("/etc/issue"):
 	image = ''
-	for text in open("/etc/issue"):
-		image += text
+	for image in open("/etc/issue"):
 		if not 'openpli' in image:
 			codec_data = {-1: ' ', 0: 'MPEG2', 1: 'MPEG4', 2: 'MPEG1', 3: 'MPEG4-II', 4: 'VC1', 5: 'VC1-SM', 6: 'HEVC', 7: ' '}
-#			gamma_data = {-1: ' ', 0: 'SDR', 1: 'HDR', 2: 'HDR10', 3: 'HLG', 4: ' '}
 		else:
+			gamma_data = {-1: ' ', 0: 'SDR', 1: 'HDR', 2: 'HDR10', 3: 'HLG', 4: ' '}
 			codec_data = {-1: ' ', 0: 'MPEG2', 1: 'AVC', 2: 'H263', 3: 'VC1', 4: 'MPEG4-VC', 5: 'VC1-SM', 6: 'MPEG1', 7: 'HEVC', 8: 'VP8', 9: 'VP9', 10: 'XVID', 11: 'N/A 11', 12: 'N/A 12', 13: 'DIVX 3', 14: 'DIVX 4', 15: 'DIVX 5', 16: 'AVS', 17: 'N/A 17', 18: 'VP6', 19: 'N/A 19', 20: 'N/A 20', 21: 'SPARK'}
 
 WIDESCREEN = [3, 4, 7, 8, 0xB, 0xC, 0xF, 0x10]
@@ -49,7 +48,7 @@ class ServiceInfoEX(Poll, Converter, object):
 	caids = 8
 	xres = 9
 	yres = 10
-#	gamma = 11
+	gamma = 11
 	atype = 12
 	vtype = 13
 	avtype = 14
@@ -109,8 +108,8 @@ class ServiceInfoEX(Poll, Converter, object):
 			self.type = self.xres
 		elif type == "yres":
 			self.type = self.yres
-#		elif type == "gamma":
-#			self.type = self.gamma
+		elif type == "gamma":
+			self.type = self.gamma
 		elif type == "atype":
 			self.type = self.atype
 		elif type == "vtype":
@@ -231,7 +230,10 @@ class ServiceInfoEX(Poll, Converter, object):
 			self.stream['yres'] = self.getServiceInfoString(info, iServiceInformation.sVideoHeight) + ("i", "p", "")[info.getInfo(iServiceInformation.sProgressive)]
 		if self.getServiceInfoString(info, iServiceInformation.sVideoWidth) != "N/A":
 			self.stream['xres'] = self.getServiceInfoString(info, iServiceInformation.sVideoWidth)
-#		self.stream['gamma'] = gamma_data[info.getInfo(iServiceInformation.sGamma)]
+		if not 'openpli' in image:
+			self.stream['gamma'] = ""
+		else:
+			self.stream['gamma'] = gamma_data[info.getInfo(iServiceInformation.sGamma)]
 		audio = service.audioTracks()
 		if audio:
 			if audio.getCurrentTrack() > -1:
@@ -278,8 +280,8 @@ class ServiceInfoEX(Poll, Converter, object):
 			streaminfo = self.stream['xres']
 		elif self.type == self.yres:
 			streaminfo = self.stream['yres']
-#		elif self.type == self.gamma:
-#			streaminfo = self.stream['gamma']
+		elif self.type == self.gamma:
+			streaminfo = self.stream['gamma']
 		elif self.type == self.atype:
 			streaminfo = self.stream['atype']
 		elif self.type == self.vtype:
@@ -297,7 +299,7 @@ class ServiceInfoEX(Poll, Converter, object):
 		elif self.type == self.volumedata:
 			streaminfo = '%s' % config.audio.volume.value
 		elif self.type == self.vsize:
-			streaminfo = self.stream['xres'] + 'x' + self.stream['yres'] + self.stream['fps']
+			streaminfo = self.stream['xres'] + 'x' + self.stream['yres'] + self.stream['fps'] + self.stream['gamma']
 		elif self.type == self.format:
 			tmp = self.sfmt[:]
 			for param in tmp.split():
